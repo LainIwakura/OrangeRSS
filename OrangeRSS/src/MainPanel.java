dbd513615b6cb010f50f0240d2c1c9800e4a7d56
miki <miki@Gunkill>
2012-04-06 20:56:32 -0400

    Added MainPanel class

---
+++ OrangeRSS/src/MainPanel.java
@@ -0,0 +1,21 @@
+/*
+ * Written by : Miki Ljuljdurovic
+ * 03-60-280 final project
+ * OrangeRss
+ */
+
+import javax.swing
+
+public class MainPanel extends JPanel {
+	
+
+	
+	
+	
+	
+	public MainPanel()
+	{
+		
+	}
+	
+}
